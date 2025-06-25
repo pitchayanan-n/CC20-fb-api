3832ae844d6011c90f424fc48b513d8e5f84768d
pitchayanan-n <pitchayanan.tf@gmail.com>
2025-06-25 15:47:21 +0700

    login done with validate try-catch

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,50 +1,118 @@
 import prisma from "../config/prisma.config.js";
 import checkIdentity from "../utils/check-identity.util.js";
 import createError from "../utils/create-error.util.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export async function register(req, res, next) {
   try {
-  const { identity, firstName, lastName, password, confirmPassword } = req.body;
-  // validation
-  if (!(identity.trim() && firstName.trim() && lastName.trim() && password.trim() && confirmPassword.trim())) {
-    createError(400, "Please fill all data")
-  }
-  if (password !== confirmPassword) {
-    createError(400, "Please check confirm password")
-  }
-  // identity เป็น email หรือ mobile phone number : checkIdentity(identity) => String : 'email' | 'mobile'
-  const identityKey = checkIdentity(identity)
+    const { identity, firstName, lastName, password, confirmPassword } =
+      req.body;
+    // validation
+    if (
+      !(
+        identity.trim() &&
+        firstName.trim() &&
+        lastName.trim() &&
+        password.trim() &&
+        confirmPassword.trim()
+      )
+    ) {
+      createError(400, "Please fill all data");
+    }
+    if (password !== confirmPassword) {
+      createError(400, "Please check confirm password");
+    }
+    // identity เป็น email หรือ mobile phone number : checkIdentity(identity) => String : 'email' | 'mobile'
+    const identityKey = checkIdentity(identity);
 
-  // หา user
-  const foundUser = await prisma.user.findUnique({
-    where: { [identityKey] : identity }
-  });
-  if (foundUser) {
-    createError(409, `Already have this user: ${identity}`)
-  }
+    // หา user
+    const foundUser = await prisma.user.findUnique({
+      where: { [identityKey]: identity },
+    });
+    if (foundUser) {
+      createError(409, `Already have this user: ${identity}`);
+    }
 
-  const newUser = {
-    [identityKey] : identity,
-    password : await bcrypt.hash(password, 10),
-    firstName : firstName,
-    lastName : lastName,
-  }
+    const newUser = {
+      [identityKey]: identity,
+      password: await bcrypt.hash(password, 10),
+      firstName: firstName,
+      lastName: lastName,
+    };
 
-  const result = await prisma.user.create({ data: newUser})
+    // const result = await prisma.user.create({ data: newUser})
 
-  res.json({
-    msg: "Register controller",
-    result: result,
-  });
-} catch (error) {
-    next(error)
+    res.json({
+      msg: "Register controller",
+      result: newUser,
+    });
+  } catch (err) {
+    next(err);
   }
 }
 
-export const login = (req, res, next) => {
+export async function registerYup(req, res, next) {
+  console.log(req.body);
+  try {
+    const { email, mobile, firstName, lastName, password } = req.body;
+    // หา user
+    if (email) {
+      let foundUserEmail = await prisma.user.findUnique({
+        where: { email: email },
+      });
+      if (foundUserEmail) {
+        createError(409, `Email : ${email} already register`);
+      }
+    }
+
+    if (mobile) {
+      let foundUserMobile = await prisma.user.findUnique({
+        where: { mobile: mobile },
+      });
+      if (foundUserMobile) {
+        createError(409, `Mobile : ${mobile} already register`);
+      }
+    }
+    const newUser = {
+      email,
+      mobile,
+      password: await bcrypt.hash(password, 10),
+      firstName,
+      lastName,
+    };
+    const result = await prisma.user.create({ data: newUser });
+
+    res.json({ msg: "Register successful", result });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export const login = async (req, res, next) => {
+  const { identity, password, email, mobile } = req.body;
+  const identityKey = email ? "email" : "mobile";
+
+  const foundUser = await prisma.user.findUnique({
+    where: { [identityKey]: identity },
+  });
+  if (!foundUser) {
+    createError(401, "Invalid login");
+  }
+  let pwOk = await bcrypt.compare(password, foundUser.password);
+  if (!pwOk) {
+    createError(401, "Invalid login");
+  }
+  // create token
+  const payload = { id: foundUser.id };
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    algorithm: "HS256",
+    expiresIn: "15d",
+  });
+
   res.json({
-    msg: "Login controller",
-    body: req.body,
+    msg: "Login successful",
+    token: token,
   });
 };
 
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,11 +1,13 @@
 import express from "express";
 // import { getMe, login, register } from "../controllers/auth.controller.js";
 import * as authController from '../controllers/auth.controller.js'
+import { loginSchema, registerSchema, validate } from "../validations/validator.js";
+import tryCatch from "../utils/try-catch.util.js";
 
 const authRoute = express.Router();
 
-authRoute.post("/login", authController.login);
-authRoute.post("/register", authController.register);
+authRoute.post("/login", validate(loginSchema), tryCatch(authController.login));
+authRoute.post("/register", validate(registerSchema), authController.registerYup);
 authRoute.get("/me", authController.getMe);
 
 export default authRoute;
